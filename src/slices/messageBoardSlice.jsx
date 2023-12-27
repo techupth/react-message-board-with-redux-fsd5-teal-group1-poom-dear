9f35890b1917bca638e35784648f3802c8ee815e
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-12-27 14:46:06 +0700

    finish with confuse

--- src/slices/messageBoardSlice.jsx
+++ src/slices/messageBoardSlice.jsx
@@ -1,1 +1,22 @@
 // TODO: Start Create Slice Here
+import { createSlice } from "@reduxjs/toolkit";
+
+export const messageSlice = createSlice({
+  name: "message",
+  initialState: [],
+  reducers: {
+    addMessage: (state, action) => {
+      state = [...state, action.payload];
+      return state;
+    },
+    deleteMessage: (state, action) => {
+      const index = action.payload;
+      state.splice(index, 1);
+
+      return state;
+    },
+  },
+});
+
+export const { addMessage, deleteMessage } = messageSlice.actions;
+export default messageSlice.reducer;
